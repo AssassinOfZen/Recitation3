a011bd75c7f994dd230a2b77e2dcfce3da153813
Owen Matimu <ragebahamutt@gmail.com>
2024-02-20 18:48:06 -0500

    Completed!

--- src/main/java/Recitation3.java
+++ src/main/java/Recitation3.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 
 public class Recitation3 {
-    public static void main(String[] argv)
-    {
+    public static void main(String[] argv) {
         Scanner scan = new Scanner(System.in);
-
-        // add your code here
+        System.out.println("Enter x: ");
+        int x = scan.nextInt();
+        System.out.println("Enter y: ");
+        int y = scan.nextInt();
+        Circle(x,y);
     }
-}
+    public static void Circle(int x, int y){
+        Circle circle16 = new Circle(new Point(x, y), 16);
+        Circle circle40 = new Circle(new Point(14, 12), 40);
+
+    if (circle16.intersects(circle40)) {
+        System.out.println("Circles intersect!");
+    } else {
+        System.out.println("Circles do not intersect!");
+    }
+
+    }
+    }
